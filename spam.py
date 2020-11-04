b82862a677e971c73cd2e38099828fd2de795f2c
asanagi <doorlessplastic630@gmail.com>
2020-11-04 13:32:02 +0800

    push

--- spam.py
+++ spam.py
@@ -3,7 +3,8 @@ import keyboard, pyperclip
 
 if __name__ == "__main__":
 
-    '''time.sleep(5)
+    # 讀取檔案每行洗版
+    time.sleep(5)
     fp = open('鮫島事件.txt')
     text = fp.readlines()
     fp.close()
@@ -11,11 +12,12 @@ if __name__ == "__main__":
     for line in text:
         pyperclip.copy(line)
         pyautogui.hotkey('command', 'v')
-        pyautogui.hotkey("enter")'''
+        pyautogui.hotkey("enter")
 
-    time.sleep(5)
+    # 無限洗版剪貼板內容，按q停止
+    '''time.sleep(5)
     while True:
         if keyboard.is_pressed('q'):
             break
         pyautogui.hotkey('command', 'v')
-        pyautogui.hotkey("enter")
+        pyautogui.hotkey("enter")'''
